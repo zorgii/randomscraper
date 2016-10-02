76c748117ab20d4b86af958a533a00fc80a4791c
zorgii <zorgi@protonmail.com>
2016-10-02 11:10:08 +0000

    code extension

--- scraper.py
+++ scraper.py
@@ -13,6 +13,27 @@ import traceback
 import bs4
 
 
+def headers(soup):
+  header3 = soup.find('h3')
+  if header3:
+    header3 = header3.get_text()
+  header4 = soup.find_all('h4')
+  if header4:
+    header4 = str(header4[1])
+    match = re.search('<h4>([\w\s\(\)-,]+)<div', header4)
+    header4 = match.group(1)
+  if header3 and header4:
+    header = header3 + '\n' + header4
+  elif header3 and not header4:
+    header = header3
+  elif header4 and not header3:
+    header = header4
+  else:
+    header = 'no header'
+  return header
+
+
+
 def imgs(soup):
   img_els = soup.find_all("img", src=True)[1:]
   img_urls = []
@@ -35,7 +56,7 @@ def tags(soup):
 
 def rate(soup):
   rating_el = str(soup.find("span", {"class" : "post_rating"}))
-  rating = re.search('<span>([\.\w]+)<div', rating_el).group(1)
+  rating = re.search('<span>([-\.\w]+)<div', rating_el).group(1)
   return rating
 
 
@@ -45,12 +66,30 @@ def user(soup):
   return username
 
 
+def download_images(img_urls, foldername):
+  pass
+
+
+
+def create_dir_txt(username, rating, header, tagnames):
+  foldername = username + ' ' + rating
+  if not os.path.isdir(foldername):
+    os.mkdir(foldername)
+  path = os.path.abspath(foldername)
+  txt = open(os.path.join(foldername, header + ".txt"), "a")
+  txt.write(tagnames)
+  txt.close()
+  return foldername
+
+
+
 def get_otherinfo(first_list):
   # ratings and uploader names
   print(len(first_list))
-  astring = first_list[-2]
+  astring = first_list[1]
   soup = bs4.BeautifulSoup(astring, 'html.parser')
 
+  header = headers(soup)
   username = user(soup)
   rating = rate(soup)
   tagnames = tags(soup)
@@ -59,7 +98,14 @@ def get_otherinfo(first_list):
   print(username)
   print(rating)
   print(tagnames)
-  print(img_urls)
+  # print(img_urls)
+  print(header)
+
+  # create folders and txt files
+  foldername = create_dir_txt(username, rating, header, tagnames)
+  
+
+  download_images(img_urls, foldername)
 
 
 
@@ -70,9 +116,6 @@ def parse_data(data):
 
 
 
-  #otherinfo = get_otherinfo(first_list)
-  #print(first_list)
-
 
 def main():
   r = requests.get('http://thatpervert.com/tag/porn%2Bcomics')
@@ -80,8 +123,6 @@ def main():
   data = r.text
   print(type(data))
   links = parse_data(data)
-  #match = re.search('c[\w]', data)
-  #print(match.group())
 
 
 if __name__ == '__main__':
